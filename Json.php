ae2a18129f4ab704752c4a5c9c1f0f203d12ec1e
Rene Koller <r.koller@me.com>
2012-08-20 16:24:37 +0200

    Json Adapter

---
+++ Json.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ *
+ * Copyright (c) 2012, René Koller
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * 1) Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * 2) Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the 
+ *    distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/** Zend_Locale */
+require_once 'Zend/Locale.php';
+
+/** Zend_Translate_Adapter */
+require_once 'Zend/Translate/Adapter.php';
+
+
+/**
+ * @category   Zend
+ * @package    Zend_Translate
+ */
+class Zend_Translate_Adapter_Json extends Zend_Translate_Adapter
+{
+    private $_data = array();
+
+	/**
+	 * Flattens an array
+	 *
+	 * @return array
+	 */
+	protected function _collapse($array)
+	{
+	    $result = array();
+	    foreach ($array as $key => $val) {
+	        if ((array)$val === $val) {
+	            foreach ($this->_collapse($val) as $nested_key => $nested_val) {
+	                $result[$key . '.' . $nested_key] = $nested_val;
+	            }
+	        } else {
+	            $result[$key] = $val;
+	        }
+	    }
+	    return $result;
+	}
+	
+    /**
+     * Load translation data
+     *
+     * @param  string|array  $data
+     * @param  string        $locale  Locale/Language to add data for, identical with locale identifier,
+     *                                see Zend_Locale for more information
+     * @param  array         $options OPTIONAL Options to use
+     * @return array
+     */
+    protected function _loadTranslationData($data, $locale, array $options = array())
+    {
+        $this->_data = array();
+        if (!file_exists($data)) {
+            require_once 'Zend/Translate/Exception.php';
+            throw new Zend_Translate_Exception("Json Translation file '". $data ."' not found");
+        }
+		
+		$jsonString = file_get_contents($data);
+		$array = json_decode($jsonString,true);
+	
+        if (!isset($this->_data[$locale])) {
+            $this->_data[$locale] = array();
+        }
+	
+		$this->_data[$locale] = $this->_collapse($array) + $this->_data[$locale];
+        return $this->_data;
+    }
+	
+    /**
+     * returns the adapters name
+     *
+     * @return string
+     */
+    public function toString()
+    {
+        return "Json";
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,40 @@ Zend-Translate-JSON-Adapter
 ===========================
 
 A JSON Adapter for the Zend Translate Module
+
+Allows the use of JSON resource files.  
+Example: 
+ 
+    {  
+		"app" : {  
+			"name" 	  : "MY AWSOME APP",
+			"version" : "1.0"
+	    },
+		
+		"errors:" {
+			"not_found" 	: "Not found",
+			"network_eror"	: "Network error"
+	    }
+    }
+	
+You can access the translations hierarchical e.g. `$translate->_("app.name")`
+
+# Initialisation #
+
+You can simply put the file into the Zend/Translate directory to initialise it like the other adapters:
+
+    $translate = new Zend_Translate(
+        array(
+            'adapter' => 'Json',
+            'content' => 'translations/de.json',
+            'locale'  => 'de'
+        )
+    );
+	
+, or follow the [Zend Translate Guidelines](framework.zend.com/manual/en/zend.translate.adapter.html#zend.translate.adapter.selfwritten) on integrating self written adapters.
+
+
+# Motivation # 
+
+I primarly wrote this adapter to use the same translation resource files for Javascript/JQuery and PHP.
+I use [i18next](http://i18next.com) for translation in Javascript in conjunction with [i18next Webtranslate](18next.com/pages/ext_webtranslate.html)
